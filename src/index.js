8b19cb884e07dfa7939afe992e712a97a9e434af
Thomas Mayrhofer <thomas@mayrhofer.at>
2016-08-15 15:33:11 +0200

    Fix eslints.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
+const toInt = (number) => parseInt(number, 10);
+
 export const fromIso = (isoString) => {
-  const seconds = parseInt(isoString.match(/[0-9]+/)[0]);
-  const minutes = parseInt(seconds / 60);
-  const hours = parseInt(minutes / 60);
-  const days = parseInt(hours / 24);
+  const seconds = toInt(isoString.match(/[0-9]+/)[0]);
+  const minutes = toInt(seconds / 60);
+  const hours = toInt(minutes / 60);
+  const days = toInt(hours / 24);
 
   return {
     seconds: seconds - (minutes * 60),
     minutes: minutes - (hours * 60),
     hours: hours - (days * 24),
     days: days,
-  }
+  };
 };
--- src/index.spec.js
+++ src/index.spec.js
@@ -10,17 +10,38 @@ const TWO_COMMON_YEAR_IN_SECONDS = ONE_COMMON_YEAR_IN_SECONDS * 2;
 
 describe('fromIso', () => {
   [
-    { isoDurationString: 'PT0S', seconds: 0 },
-    { isoDurationString: 'PT6S', seconds: 6 },
-    { isoDurationString: 'PT7S', seconds: 7 },
-    { isoDurationString: 'PT10S', seconds: 10 },
-    { isoDurationString: 'PT59S', seconds: 59 },
-    { isoDurationString: `PT${ONE_MINUTE_IN_SECONDS}S`, seconds: 0, minutes: 1 },
-    { isoDurationString: `PT${ONE_HOUR_IN_SECONDS}S`, seconds: 0, minutes: 0, hours: 1 },
-    { isoDurationString: `PT${ONE_DAY_IN_SECONDS}S`, seconds: 0, minutes: 0, hours: 0, days: 1},
-    { isoDurationString: `PT${ONE_COMMON_YEAR_IN_SECONDS}S`, seconds: 0, minutes: 0, hours: 0, days: 365 },
-    { isoDurationString: `PT${TWO_COMMON_YEAR_IN_SECONDS}S`, seconds: 0, minutes: 0, hours: 0, days: 730 },
-  ].forEach(({ isoDurationString, seconds = 0, minutes = 0, hours = 0, days = 0}) => {
+    {
+      isoDurationString: 'PT0S',
+      seconds: 0,
+    }, {
+      isoDurationString: 'PT6S',
+      seconds: 6,
+    }, {
+      isoDurationString: 'PT7S',
+      seconds: 7,
+    }, {
+      isoDurationString: 'PT10S',
+      seconds: 10,
+    }, {
+      isoDurationString: 'PT59S',
+      seconds: 59,
+    }, {
+      isoDurationString: `PT${ONE_MINUTE_IN_SECONDS}S`,
+      minutes: 1,
+    }, {
+      isoDurationString: `PT${ONE_HOUR_IN_SECONDS}S`,
+      hours: 1,
+    }, {
+      isoDurationString: `PT${ONE_DAY_IN_SECONDS}S`,
+      days: 1,
+    }, {
+      isoDurationString: `PT${ONE_COMMON_YEAR_IN_SECONDS}S`,
+      days: 365,
+    }, {
+      isoDurationString: `PT${TWO_COMMON_YEAR_IN_SECONDS}S`,
+      days: 730,
+    },
+  ].forEach(({ isoDurationString, seconds = 0, minutes = 0, hours = 0, days = 0 }) => {
     describe(`${isoDurationString} responds`, () => {
       it(`${seconds} seconds`, () => assertThat(
         fromIso(isoDurationString).seconds, equalTo(seconds)));
@@ -36,5 +57,3 @@ describe('fromIso', () => {
     });
   });
 });
-
-
