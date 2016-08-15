20b49c830ad00a38baf12b6762a553999dcad806
Thomas Mayrhofer <thomas@mayrhofer.at>
2016-08-15 15:15:50 +0200

    Hours are calculated correctly.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 export const fromIso = (isoString) => {
   const seconds = parseInt(isoString.match(/[0-9]+/)[0]);
   const minutes = parseInt(seconds / 60);
+  const hours = parseInt(minutes / 60);
 
   return {
     seconds: seconds - (minutes * 60),
-    minutes: minutes
+    minutes: minutes - (hours * 60),
+    hours: hours
   }
 };
--- src/index.spec.js
+++ src/index.spec.js
@@ -1,7 +1,7 @@
 import { assertThat, equalTo } from 'hamjest';
 import { fromIso } from './index';
 
-describe('ISO duration to seconds', () => {
+describe('fromIso', () => {
   [
     { isoDurationString: 'PT0S', seconds: 0 },
     { isoDurationString: 'PT6S', seconds: 6 },
@@ -9,6 +9,8 @@ describe('ISO duration to seconds', () => {
     { isoDurationString: 'PT10S', seconds: 10 },
     { isoDurationString: 'PT59S', seconds: 59 },
     { isoDurationString: 'PT60S', seconds: 0, minutes: 1 },
+    { isoDurationString: 'PT61S', seconds: 1, minutes: 1 },
+    { isoDurationString: 'PT3600S', seconds: 0, minutes: 0, hours: 1 },
   ].forEach(({ isoDurationString, seconds = 0, minutes = 0}) => {
     describe(`${isoDurationString} responds`, () => {
       it(`${seconds} seconds`, () => {
