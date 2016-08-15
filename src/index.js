e3d97dbf6d131b4339c2f53a3096965f3deb7cb9
Thomas Mayrhofer <thomas@mayrhofer.at>
2016-08-15 15:10:26 +0200

    Minutes are calculated correctly

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 export const fromIso = (isoString) => {
   const seconds = parseInt(isoString.match(/[0-9]+/)[0]);
+  const minutes = parseInt(seconds / 60);
+
   return {
-    seconds: seconds
+    seconds: seconds - (minutes * 60),
+    minutes: minutes
   }
 };
--- src/index.spec.js
+++ src/index.spec.js
@@ -8,10 +8,16 @@ describe('ISO duration to seconds', () => {
     { isoDurationString: 'PT7S', seconds: 7 },
     { isoDurationString: 'PT10S', seconds: 10 },
     { isoDurationString: 'PT59S', seconds: 59 },
+    { isoDurationString: 'PT60S', seconds: 0, minutes: 1 },
+  ].forEach(({ isoDurationString, seconds = 0, minutes = 0}) => {
+    describe(`${isoDurationString} responds`, () => {
+      it(`${seconds} seconds`, () => {
+        assertThat(fromIso(isoDurationString).seconds, equalTo(seconds));
+      });
 
-  ].forEach(({ isoDurationString, seconds }) => {
-    it(`${isoDurationString} responds ${seconds} seconds`, () => {
-      assertThat(fromIso(isoDurationString).seconds, equalTo(seconds));
+      it(`${minutes} minutes`, () => {
+        assertThat(fromIso(isoDurationString).minutes, equalTo(minutes));
+      });
     });
   });
 });
