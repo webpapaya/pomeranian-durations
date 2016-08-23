5e2b6c5fb54a6c4333fa4bb7affe576ce1fcc43d
Thomas Mayrhofer <thomas@mayrhofer.at>
2016-08-23 08:30:56 +0200

    Add test to untested toFragments function.

--- src/transformations.spec.js
+++ src/transformations.spec.js
@@ -1,7 +1,20 @@
 import { assertThat, equalTo } from 'hamjest';
-import { toIso } from './transformations';
+import { toIso, toFragments } from './transformations';
 
-describe('fromFragments', () => {
-  it('toIso responds correct format', () => assertThat(
+describe('toIso', () => {
+  it('responds correct format', () => assertThat(
     toIso({ years: 1, days: 2, hours: 1, minutes: 66 }), equalTo('P1Y2DT1H66M')));
+});
+
+describe('toFragments', () => {
+  it('transforms iso string to fragments', () => assertThat(
+    toFragments('P1Y2DT1H66M'), equalTo({
+      years: 1,
+      months: 0,
+      weeks: 0,
+      days: 2,
+      hours: 1,
+      minutes: 66,
+      seconds: 0,
+    })));
 });
