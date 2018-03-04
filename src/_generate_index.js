9301686ee857827009b090355f57b885dbb88fba
Thomas Mayrhofer <thomas@mayrhofer.at>
2018-03-04 18:59:38 +0100

    Add newline at the end of the file.

--- src/_generate_index.js
+++ src/_generate_index.js
@@ -7,4 +7,4 @@ fs.unlinkSync(INDEX_PATH);
 const files = fs.readdirSync(DIRECTORY).filter((file) => file[0] !== '_' && !file.includes('.spec.js'));
 
 const indexContent = files.map((file) => `export * from './${file}';`).join('\n');
-fs.writeFileSync(INDEX_PATH, indexContent);
+fs.writeFileSync(INDEX_PATH, `${indexContent}\n`);
