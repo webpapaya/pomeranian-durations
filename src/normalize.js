a8aaee7320bef36da5cb60ff61a0907205321023
Thomas Mayrhofer <thomas@mayrhofer.at>
2018-05-03 19:44:08 +0200

    Added normalizeTime function.

---
+++ src/normalize.js
@@ -0,0 +1,34 @@
+/**
+ * Helpers to normalize an ISO8601 duration. (eg. 61 seconds => 1 minute 1 second)
+ * @name default
+ */
+
+import { INVALID_DURATION, ONE_HOUR, ONE_MINUTE, ONE_SECOND, UNIT_NAMES } from "./constants";
+import { toFragments, toIso } from './transformations';
+import { asMicroseconds } from './conversions';
+import { isInvalid } from './validate';
+import { pick, pipe } from "./_utils";
+
+const TIME_UNITS = [UNIT_NAMES.hours, UNIT_NAMES.minutes, UNIT_NAMES.seconds];
+
+/**
+ * Normalizes the time part of an iso8601 duration.
+ * @param isoString {string} - an ISO8601 duration
+ * @returns isoString {string} - an ISO8601 duration
+ * @example
+ * normalizeTime('P1DT1234S') // => 'P1DT20M34S'
+ * @example
+ * normalizeTime('PT1S') // => 'PT1S'
+ */
+export const normalizeTime = (isoString) => {
+  if (isInvalid(isoString)) { return INVALID_DURATION; }
+
+  const fragments = toFragments(isoString);
+  const microseconds = pipe(fragments, pick(TIME_UNITS), toIso, asMicroseconds);
+
+  const hour = Math.floor(microseconds / ONE_HOUR);
+  const minutes = Math.floor((microseconds - hour * ONE_HOUR) / ONE_MINUTE);
+  const seconds = (microseconds - hour * ONE_HOUR - minutes * ONE_MINUTE) / ONE_SECOND;
+
+  return toIso({ ...fragments, hour, minutes, seconds});
+};
---
+++ src/normalize.spec.js
@@ -0,0 +1,14 @@
+import { assertThat, equalTo } from 'hamjest';
+import { normalizeTime } from './normalize';
+import { INVALID_DURATION } from "./constants";
+
+describe('normalizeTime converts', () => {
+  [
+    { input: 'INVALID', result: INVALID_DURATION },
+    { input: 'PT1111S', result: 'PT18M31S' },
+  ].forEach(({ input, result }) => {
+    it(`${input} to ${result}`, () => {
+      assertThat(normalizeTime(input), equalTo(result));
+    });
+  });
+});
