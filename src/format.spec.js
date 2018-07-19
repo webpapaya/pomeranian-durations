42772eb3f264a892dea9169c3202249fcec150bb
Thomas Mayrhofer <thomas@mayrhofer.at>
2018-07-19 09:46:13 +0200

    All date parts are uppercase now.

--- src/format.spec.js
+++ src/format.spec.js
@@ -5,25 +5,25 @@ const testFormat = (token, isoString, result) =>
   it(`${token}`, () => assertThat(format(token, isoString), equalTo(result)));
 
 describe('format', () => {
-  testFormat('%y', 'P', '0');
-  testFormat('%y', 'P1Y', '1');
-  testFormat('%yy', 'P', '00');
-  testFormat('%yy', 'P1Y', '01');
+  testFormat('%Y', 'P', '0');
+  testFormat('%Y', 'P1Y', '1');
+  testFormat('%YY', 'P', '00');
+  testFormat('%YY', 'P1Y', '01');
 
   testFormat('%M', 'P', '0');
   testFormat('%M', 'P1M', '1');
   testFormat('%MM', 'P', '00');
   testFormat('%MM', 'P1M', '01');
 
-  testFormat('%w', 'P', '0');
-  testFormat('%w', 'P1W', '1');
-  testFormat('%ww', 'P', '00');
-  testFormat('%ww', 'P1W', '01');
+  testFormat('%W', 'P', '0');
+  testFormat('%W', 'P1W', '1');
+  testFormat('%WW', 'P', '00');
+  testFormat('%WW', 'P1W', '01');
 
-  testFormat('%d', 'P', '0');
-  testFormat('%d', 'P1D', '1');
-  testFormat('%dd', 'P', '00');
-  testFormat('%dd', 'P1D', '01');
+  testFormat('%D', 'P', '0');
+  testFormat('%D', 'P1D', '1');
+  testFormat('%DD', 'P', '00');
+  testFormat('%DD', 'P1D', '01');
 
   testFormat('%h', 'P', '0');
   testFormat('%h', 'PT1H', '1');
